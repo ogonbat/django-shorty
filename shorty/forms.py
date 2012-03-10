56f47356ae8a29695ccaa98fc062e258b764a7c3
Andrea Mucci <cingusoft@gmail.com>
2012-03-10 21:12:29 +0100

    personal slug restructured, indendation and some bugs

--- shorty/forms.py
+++ shorty/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from shorty.models import Pslug
 
 __author__ = 'ogonbat'
 
@@ -10,11 +9,4 @@ class Private_Form(forms.Form):
 class Shorty_Form(forms.Form):
     link_url = forms.URLField()
     personal_slug = forms.CharField(max_length=20,required=False)
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        try:
-            slug_unique = Pslug.objects.get(slug__exact=cleaned_data['personal_slug'])
-        except Pslug.DoesNotExist:
-            return cleaned_data
-        raise forms.ValidationError("The personal Slug already exist")
+    private_password = forms.CharField(widget=forms.PasswordInput(),required=False)
--- shorty/views.py
+++ shorty/views.py
@@ -5,36 +5,46 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from shorty.decorators import shorty_filter_url, shorty_login_required
 from shorty.forms import Private_Form, Shorty_Form
-from shorty.models import Url, Pslug
+from shorty.models import Url
 from shorty.utils import url_encode
 
 @shorty_filter_url
 def shorty_url(request,url_object,private_template='shorty/private.html'):
+    
     """ Usage
         urlpatterns = patterns('',
                 url(r'^(?P<slug>[\a-zA-Z0-9\-]+)/$','shorty.views.shorty_url',{'private_template':'your_private_template.html'},name="shorty_url"),
                 )
     """
     if url_object.private == True:
+        
         #check the form submission
         if request.method == 'POST':
             #this is the private form post
             form = Private_Form(data=request.POST)
+        
             if form.is_valid():
                 #check the private password
                 if request.POST.get('private_password','') == url_object.private_password:
                     return HttpResponsePermanentRedirect(url_object.url_field)
+            
             else:
                 data_context = {
                     'shorty_form': form
                 }
+            
                 return render_to_response(private_template, data_context,context_instance=RequestContext(request))
+        
         else:
+        
             form = Private_Form()
+        
         data_context = {
             'shorty_form': form
         }
+        
         return render_to_response(private_template, data_context,context_instance=RequestContext(request))
+    
     return HttpResponsePermanentRedirect(url_object.url_field)
 
 @shorty_login_required
@@ -46,32 +56,48 @@ def add_shorty_url(request,shorty_template='shorty/add.html'):
                 )
     """
     if request.method == 'POST':
+        
         #added new link
         form = Shorty_Form(data=request.POST)
+        
         if form.is_valid():
             #the form is valid, add new form
             model_shorty = Url()
+            
             if request.user.is_authenticated():
                 model_shorty.user = request.user
+            
             model_shorty.url_field = request.POST.get('link_url','')
             model_shorty.status = 'Active'
-            model_shorty.save()
+            
+            if request.POST.get('private_password'):
+                model_shorty.private = True
+                model_shorty.private_password = request.POST.get('private_password')
+                
             if request.POST.get('personal_slug'):
+                model_shorty.personal_slug = request.POST.get('personal_slug')
                 short_url = request.POST.get('personal_slug')
             else:
                 short_url = url_encode(model_shorty.id)
+                
             data_context = {
                 'shorty_form': form,
                 'url_slug': short_url,
             }
+            model_shorty.save()
+        
         else:
             data_context = {
                 'shorty_form': form,
             }
+            
         return render_to_response(shorty_template,data_context,context_instance=RequestContext(request))
+    
     else:
+        
         form = Shorty_Form()
         data_context = {
             'shorty_form': form
         }
+        
         return render_to_response(shorty_template,data_context,context_instance=RequestContext(request))
